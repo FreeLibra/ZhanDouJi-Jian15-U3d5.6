e183e99da0321265b4e4c2e7712656fc26ae8fb2
syq <shenyongqiang2008@126.com>
2017-07-20 14:26:03 +0800

    修改UV控制脚本

--- Client/UVA.cs
+++ Client/UVA.cs
@@ -2,21 +2,38 @@
 using System.Collections;
 
 public class UVA : MonoBehaviour {
-	public float speed=0.5f;
-	public int Array=0;
-	// Use this for initialization
-	void Start () {
-		
+	[Range(1, 100)]public int CountImg = 1;
+	public float Speed = 0.5f;
+	/**
+	 * 改变UV的材质序号.
+	 */
+	public int ArrayIndex = 0;
+	Material UVMaterial;
+	public float MinOffset;
+	int Count;
+
+	// Use this for initialization.
+	void Start()
+	{
+		MinOffset = 1f / CountImg;
+		UVMaterial = GetComponent<Renderer>().materials[ArrayIndex];
+		UVMaterial.SetTextureScale("_MainTex", new Vector2 (MinOffset, 1f));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//		bool s=true;
-		
-		float offset = Time.time *speed;
-		//		Debug.Log (offset);
-		GetComponent<Renderer>().materials[Array].SetTextureOffset("_MainTex",new Vector2 (offset,0));
-		
-		
+
+	// Update is called once per frame.
+	void Update()
+	{
+		float offset = Time.time * Speed;
+		if (CountImg > 1) {
+			float offsetTmp = MinOffset * Count;
+			if (offset < offsetTmp) {
+				offset = offsetTmp;
+			}
+			else {
+				Count++;
+				offset = MinOffset * Count;
+			}	
+		}
+		UVMaterial.SetTextureOffset("_MainTex", new Vector2 (offset, 0f));
 	}
 }
